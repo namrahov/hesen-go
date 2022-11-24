aab2dd52da4e7aaa383ea207aace8951e6a91dd3
namrahov <amrahovnurlan@gmail.com>
2022-11-24 22:19:18 +0400

    7th commit

--- example/main.go
+++ example/main.go
@@ -2,44 +2,31 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
+
+var ch = make(chan string)
 
 func main() {
 
 	stratTime := time.Now()
 
-	var wg sync.WaitGroup
+	go count()
+	go hesen()
 
-	wg.Add(2)
-	go count(1, &wg)
-	go hesen(2, &wg)
-
-	wg.Wait()
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 
 	fmt.Println(time.Now().Sub(stratTime))
 	fmt.Println("Main thread done")
 }
 
-func count(a int, wg *sync.WaitGroup) {
-
-	/*for i := 0; i < 10; i++ {
-		fmt.Println("Thread a=", a, " ", i)
-	}*/
-
-	time.Sleep(3 * time.Second)
-	wg.Done()
-
+func count() {
+	time.Sleep(2 * time.Second)
+	ch <- "count isledi"
 }
 
-func hesen(a int, wg *sync.WaitGroup) {
-
-	/*for i := 0; i < 10; i++ {
-		fmt.Println("Thread a=", a, " ", i)
-	}*/
-
-	time.Sleep(4 * time.Second)
-	wg.Done()
-
+func hesen() {
+	time.Sleep(2 * time.Second)
+	ch <- "hesen isledi"
 }
